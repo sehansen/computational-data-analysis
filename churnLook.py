e56b1fb33dfdbe0c13321ec541153111f50d7055
deoxyribose <frzdyb@gmail.com>
2013-02-28 16:35:25 +0100

    preprocessing and exploration

--- churnLook.py
+++ churnLook.py
@@ -4,6 +4,10 @@ from sklearn.decomposition  import PCA
 from sklearn.preprocessing  import scale
 import matplotlib.pyplot as plt
 t = transpose
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 8}
+plt.rc('font', **font)
 
 churnFile = loadmat("churn_work.mat")
 
@@ -16,7 +20,6 @@ def month_transform(m):
     if 0 < m and m < 8: return m + 12
     else: return m
 churnGeneralMod = concatenate([churnGeneral[:,0:4], t(atleast_2d(map(month_transform,churnGeneral[:,4]))), churnGeneral[:,5:]],1)
-
 generalPCA = PCA()
 
 generalPCA.fit(scale(churnGeneralMod))
@@ -26,29 +29,31 @@ tran = generalPCA.transform(churnGeneralMod)
 # print generalPCA.components_[0]
 # print generalPCA.explained_variance_ratio_
 
+# filter out customers with status 0
 churnFilter = nonzero(churnGeneral[:,-2])
 churnFiltered = churnGeneral[churnFilter,:][0]
-for i in xrange(1,20):
-            plt.subplot(4,5,i+1)
-            plt.plot(churnFiltered[:,0],churnFiltered[:,i],'.')
-            plt.xticks([])
-            plt.yticks([])
-            plt.title(churnGNames[i])
-plt.figure()
-churnFilter = nonzero(1 - churnGeneral[:,-2])
-churnFiltered = churnGeneral[churnFilter,:][0]
-for i in xrange(1,20):
-            plt.subplot(4,5,i+1)
-            plt.plot(churnFiltered[:,0],churnFiltered[:,i],'r.')
-            plt.xticks([])
-            plt.yticks([])
-            plt.title(churnGNames[i])
-
-# for i in xrange(7):
-#     for j in xrange(7):
-#         if i != j:
-#             plt.subplot(20,20,i*20+j+1)
-#             plt.plot(churnGeneral[:,i],churnGeneral[:,j],'.')
+churnFiltered.view('float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64,float64').sort(order=['f0'], axis=0)
+# for i in xrange(1,20):
+#             plt.subplot(4,5,i+1)
+#             plt.plot(churnFiltered[:,0],churnFiltered[:,i],'.')
 #             plt.xticks([])
 #             plt.yticks([])
-plt.show()
+#             plt.title(churnGNames[i])
+# plt.figure()
+# churnFilter = nonzero(1 - churnGeneral[:,-2])
+# churnFiltered = churnGeneral[churnFilter,:][0]
+# for i in xrange(1,20):
+#             plt.subplot(4,5,i+1)
+#             plt.plot(churnFiltered[:,0],churnFiltered[:,i],'r.')
+#             plt.xticks([])
+#             plt.yticks([])
+#             plt.title(churnGNames[i][0])
+
+# # for i in xrange(7):
+# #     for j in xrange(7):
+# #         if i != j:
+# #             plt.subplot(20,20,i*20+j+1)
+# #             plt.plot(churnGeneral[:,i],churnGeneral[:,j],'.')
+# #             plt.xticks([])
+# #             plt.yticks([])
+# plt.savefig("initplot.png")
